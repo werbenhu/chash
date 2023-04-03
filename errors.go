510a8049fab992e8c228ebf6f897efc452042402
werben <werben@aliyun.com>
2023-04-03 21:52:59 +0800

    fixed errors.go
    fixed errors.go

--- errors.go
+++ errors.go
@@ -19,6 +19,6 @@ func (e err) Error() string {
 
 var (
 	ErrBucketNotFound  = err{Code: 10000, Msg: "bucket not found"}
-	ErrBucketExisted   = err{Code: 10000, Msg: "bucket already existed"}
-	ErrNoResultMatched = err{Code: 10001, Msg: "no result matched"}
+	ErrBucketExisted   = err{Code: 10001, Msg: "bucket already existed"}
+	ErrNoResultMatched = err{Code: 10002, Msg: "no result matched"}
 )
